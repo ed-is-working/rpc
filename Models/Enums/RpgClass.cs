c0984eb98744ef55d4bf88494fe49490b5778ec2
Edward Apostol <ed@edapostol.com>
2023-08-13 08:40:47 -0400

    feat: created default Enum for roles / types

---
+++ Models/Enums/RpgClass.cs
@@ -0,0 +1,12 @@
+namespace rpc.Models.Enums
+{
+    public enum RpgClass
+    {
+       Knight = 1,
+       Mage = 2,
+       Cleric = 3,
+       NPC = 4,
+       Admin = 10000,
+       User =  9999,
+    }
+}
