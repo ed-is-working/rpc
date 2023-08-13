5bf316d6a3495b8326eb45e3d5a29517955a1e18
Edward Apostol <ed@edapostol.com>
2023-08-13 17:44:03 -0400

    WIP: created initial Character Service

---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using rpc.Models;
+
+namespace rpc.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        // enable list of characters
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character { Id = 1, Name = "Sam" }
+        };
+        public List<Character> AddCharacter(Character newCharacter)
+        {
+
+            // TODO: add validation, ensure that a character with the same name / Id does not already exist
+            characters.Add(newCharacter);
+            return characters;
+        }
+
+        public List<Character> GetAllCharacters()
+        {
+            // can also implement BadRequest or NotFound
+            return characters;
+        }
+
+        public Character GetCharacterById(int id)
+        {
+            // can also implement BadRequest or NotFound
+            return characters.FirstOrDefault(c => c.Id == id);
+        }
+
+    }
+}
