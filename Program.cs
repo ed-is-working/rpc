c07d2af979f6a2e88742f1645e5e36f6362f5d81
Edward Apostol <ed@edapostol.com>
2023-08-14 15:45:05 -0400

    Add AuthRepository Service to app container

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 global using rpc.Models;
 global using rpc.Services.CharacterService;
 global using rpc.DTOs.Character;
-global using AutoMapper; // It is used in the CharacterService.cs and will be used elsewhere
-global using Microsoft.EntityFrameworkCore; // It is used in the DataContext.cs and will be used elsewhere
-global using rpc.Data; // It is used in the DataContext.cs and will be used elsewhere
+global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using rpc.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,8 +20,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 // Add the CharacterService to the container
-// add a scoped service for every request
 builder.Services.AddScoped<ICharacterService, CharacterService>();
+
+// Add the AuthRepository to the container
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 
 var app = builder.Build();
 
