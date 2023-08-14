99a006d22c9e01d83c5e7424fb5c71159ebbdbf0
Edward Apostol <ed@edapostol.com>
2023-08-13 21:37:54 -0400

    feat: refactor character service to ensure usage of character DTOs

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -16,10 +16,10 @@ namespace rpc.Services.CharacterService
         };
 
         /* TODO: when DB is implemented, update this to use DB with await calls */
-        public async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
+        public async Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter)
         {
             // add serviceResponse to every method and set the data property accordingly
-            var serviceResponse = new ServiceResponse<List<Character>>();
+            var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
 
             // TODO: add validation, ensure that a character with the same name / Id does not already exist
             characters.Add(newCharacter);
@@ -27,30 +27,30 @@ namespace rpc.Services.CharacterService
             return serviceResponse;
         }
 
-        public async Task<ServiceResponse<List<Character>>> GetAllCharacters()
+        public async Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters()
         {
             // add serviceResponse to every method and set the data property accordingly
-            var serviceResponse = new ServiceResponse<List<Character>>();
+            var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
             serviceResponse.Data = characters;  // set the data property to the list of characters
 
             // can also implement BadRequest or NotFound
             return serviceResponse;
         }
 
-        public async Task<ServiceResponse<Character>> GetCharacterById(int id)
+        public async Task<ServiceResponse<GetCharacterDTO>> GetCharacterById(int id)
         {
             var character = characters.FirstOrDefault(c => c.Id == id);
 
             // can also implement BadRequest or NotFound
             // null check is now removed since data is nullable, will return null
-            var serviceResponse = new ServiceResponse<Character>();
+            var serviceResponse = new ServiceResponse<GetCharacterDTO>();
             // set character to the character with the matching id
             character = characters.FirstOrDefault(c => c.Id == id); 
             serviceResponse.Data = character;
             return serviceResponse;
         }
 
-        Task<ServiceResponse<Character>> ICharacterService.GetCharacterById(int id)
+        Task<ServiceResponse<GetCharacterDTO>> ICharacterService.GetCharacterById(int id)
         {
             throw new NotImplementedException();
         }
