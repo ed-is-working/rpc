2f4afee4b8f09a25d49100b466d7b7211e38d946
Edward Apostol <ed@edapostol.com>
2023-08-13 21:37:31 -0400

    feat: ensure character interface uses characterDTOs

--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using rpc.DTOs.Character;
 using rpc.Models;
 
 namespace rpc.Services.CharacterService
@@ -9,8 +10,8 @@ namespace rpc.Services.CharacterService
     public interface ICharacterService
     {
         // refactored to use ServiceResponse, which is a generic class
-        Task<ServiceResponse<List<Character>>> GetAllCharacters();
-        Task<ServiceResponse<Character>> GetCharacterById(int id);
-        Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter);
+        Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters();
+        Task<ServiceResponse<GetCharacterDTO>> GetCharacterById(int id);
+        Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter);
     }
 }
