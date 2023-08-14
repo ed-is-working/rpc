7d287a5bbce04e5e239d82de069881cc9612bd27
Edward Apostol <ed@edapostol.com>
2023-08-14 14:53:50 -0400

    feat: associate Users table to store user models

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -16,6 +16,7 @@ namespace rpc.Data
         // This is the table that will be created in the database
         // public DbSet<Character> Characters { get; set; } - or -
         public DbSet<Character> Characters => Set<Character>(); // This is the same as above
+        public DbSet<User> Users => Set<User>();
         
         
     }
