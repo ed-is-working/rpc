7f018145ad85f4338923f64c8d583443a2f7f937
Edward Apostol <ed@edapostol.com>
2023-08-15 13:17:48 -0400

    feat: added JSONWebToken creation method

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -1,17 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
 
 namespace rpc.Data
 {
     public class AuthRepository : IAuthRepository
     {
         private readonly DataContext _context;
+        private readonly IConfiguration _config;
 
-        public AuthRepository(DataContext context)
+        public AuthRepository(DataContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
         public async Task<ServiceResponse<string>> Login(string username, string password)
         {
@@ -29,7 +35,8 @@ namespace rpc.Data
                 response.Message = "Incorrect password.";
             } else
             {
-                response.Data = user.Id.ToString();
+                // response.Data = user.Id.ToString();
+                response.Data = CreateToken(user);
             }
 
             return response;
@@ -80,12 +87,12 @@ namespace rpc.Data
                 // set the salt to the randomly generated key
                 passwordSalt = hmac.Key;
                 // set the hash to the computed hash of the password
-                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
             }
         }
     
         // method to verify the password
-        private bool VerifyPasswordHash(
+        private static bool VerifyPasswordHash(
             string password,
             byte[] passwordHash,
             byte[] passwordSalt)
@@ -94,12 +101,55 @@ namespace rpc.Data
             using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
             {
                 // set the hash to the computed hash of the password
-                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
                 return computedHash.SequenceEqual(passwordHash);
 
             }
         } 
-    
+
+        private string CreateToken(User user)
+        {
+
+            // create the list of claims
+            /* 
+                claims in JWT Tokens are used to store information about the user
+                e.g. the user id, username, etc. in the token payload, as well as
+                the expiration date of the token and iAt (issued at) date
+            */
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+
+            };
+
+            // create the key, obtaining the Secret Key from the AppSettings
+            var appSettingsToken = _config.GetSection("AppSettings:Token").Value;
+            if (appSettingsToken is null)
+            {
+                throw new ArgumentNullException("AppSettings Token is null");
+
+            }
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(appSettingsToken));
+
+            // create the signing credentials
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // create the token descriptor that contains the claims, expiration date, and signing credentials
+            // (a placeholder for all the attributes related to the final issued token)
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1), // token expires in 1 day
+                SigningCredentials = creds
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // serialize the token into a JSONWebToken string
+            return tokenHandler.WriteToken(token);
     
     }
 }
+}  // end of namespace
